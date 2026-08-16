9a49ee8c95dc30620998755355cdbb1af228a75a
Michel Silveira <michel.proelium@gmail.com>
2026-08-15 22:05:51 -0300

    fix: fixar seletor de ambientes no celular

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'proelium-shell-v83';
+const CACHE = 'proelium-shell-v84';
 const ASSETS = ['./','./index.html','./styles.css','./crm.css','./bi.css','./quotes.css','./danger.css','./app.js','./manifest.webmanifest','./icon.svg','./assets/ems-studio-logo.png'];
 
 self.addEventListener('install', event => {
