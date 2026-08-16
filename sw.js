a91f13060add50b6c4af9df7abd20d857db45f4a
Michel Silveira <michel.proelium@gmail.com>
2026-08-16 01:17:13 -0300

    feat: adicionar levantamento técnico

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'proelium-shell-v130';
+const CACHE = 'proelium-shell-v131';
 const ASSETS = ['./','./index.html','./styles.css','./crm.css','./bi.css','./quotes.css','./danger.css','./app.js','./manifest.webmanifest','./icon.svg','./assets/ems-studio-logo.png'];
 
 self.addEventListener('install', event => {
